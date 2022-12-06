3b5311ed182e87f56cf87a4c149ac088456bceee
Ross Kimes <ross@rosskimes.net>
2022-12-06 08:46:28 -0600

    Added Collection.at(index:).

---
+++ Sources/Terminus/Foundation/Collection.swift
@@ -0,0 +1,15 @@
+//
+//  Collection.swift
+//  
+//
+//  Created by Ross Kimes on 12/6/22.
+//
+
+import Foundation
+
+extension Collection {
+	
+	func at(index: Index) -> Element? {
+		indices.contains(index) ? self[index] : nil
+	}
+}
